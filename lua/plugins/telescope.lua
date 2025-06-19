d1573cb08f76e9c0aff65e46ec8817299be85512
Jack J <jjack@Jacks-Air.broadband>
2025-06-19 16:10:52 +0100

    File dedicated for telescope

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,43 @@
+-- For telescope and for neo-tree
+
+return {
+    {
+        "nvim-telescope/telescope.nvim",
+        event = "VeryLazy",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        keys = {
+            {
+                "<leader>ff",
+                "<cmd>Telescope find_files <CR>",
+                desc = "Find Files",
+            },
+            {
+                "<leader>fa",
+                "<cmd>Telescope find_files follow=true no_ignore=true hidden=true <CR>",
+                desc = "Find All Files",
+            },
+            {
+                "<leader>fg",
+                "<cmd>Telescope live_grep <CR>",
+                desc = "Grep",
+            },
+            {
+                "<leader>fh",
+                "<cmd>Telescope help_tags <CR>",
+                desc = "Help Tags",
+            },
+            {
+                "<leader>fo",
+                "<cmd>Telescope oldfiles <CR>",
+                desc = "Old files",
+            },
+            {
+                "<leader>fc",
+                "<cmd>Telescope colorscheme <CR>",
+                desc = "Color Scheme",
+            },
+        },
+    },
+}
